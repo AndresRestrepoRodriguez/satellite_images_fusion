f62e954d2e9054e5963c9875a2b93def885559dd
Andres Restrepo <arestrepo@vozy.co>
2022-03-26 18:53:26 -0500

    Fix invoker imports

--- invoker.py
+++ invoker.py
@@ -1,12 +1,13 @@
-from .algorithms import gram_fusion_cpu, gram_fusion_gpu, high_pass_fusion_cpu, high_pass_fusion_gpu, hpf_fusion_cpu, hpf_fusion_gpu, mean_value_fusion_cpu, mean_value_fusion_gpu
+from .algorithms import gram_fusion_cpu, gram_fusion_gpu, high_pass_fusion_cpu, high_pass_fusion_gpu, hpf_fusion_cpu,\
+    hpf_fusion_gpu, mean_value_fusion_cpu, mean_value_fusion_gpu
 from .utils import utils
 
 
 METHODS_FUSION = {
-                    'hpf': {'cpu': fusion_hpf_cpu, 'gpu': fusion_hpf_gpu},
-                    'mean_value': {'cpu': fusion_valor_medio_cpu, 'gpu': fusion_valor_medio_gpu},
-                    'high_pass': {'cpu': fusion_paso_alto_cpu, 'gpu':fusion_paso_alto_gpu},
-                    'gram': {'cpu': fusion_gram_cpu, 'gpu': fusion_gram_gpu}
+                    'hpf': {'cpu': hpf_fusion_cpu.fusion_hpf_cpu, 'gpu': hpf_fusion_gpu.fusion_hpf_gpu},
+                    'mean_value': {'cpu': mean_value_fusion_cpu.fusion_valor_medio_cpu, 'gpu': mean_value_fusion_gpu.fusion_valor_medio_gpu},
+                    'high_pass': {'cpu': high_pass_fusion_cpu.fusion_paso_alto_cpu, 'gpu':high_pass_fusion_gpu.fusion_paso_alto_gpu},
+                    'gram': {'cpu': gram_fusion_cpu.fusion_gram_cpu, 'gpu': gram_fusion_gpu.fusion_gram_gpu}
                   }
 
 
