6a9410ebfd53a0ab5bcc288018f5798850c6f0c6
Andres Restrepo <andres@ANDRES.localdomain>
2022-07-31 18:26:17 -0500

    fix by init v3

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
     name = 'satellite_images_fusion',
     packages = ['satellite_images_fusion'],
-    version = '1.0.6',  # Ideally should be same as your GitHub release tag varsion
+    version = '1.0.7',  # Ideally should be same as your GitHub release tag varsion
     description = 'Package for satellite image fusion',
     author = 'ParallUD',
     author_email = 'aorestrepor@correo.udistrital.edu.co',
     url = 'https://github.com/AndresRestrepoRodriguez/satellite_images_fusion',
-    download_url = 'https://github.com/AndresRestrepoRodriguez/satellite_images_fusion/archive/refs/tags/1.0.6.tar.gz',
+    download_url = 'https://github.com/AndresRestrepoRodriguez/satellite_images_fusion/archive/refs/tags/1.0.7.tar.gz',
     keywords = ['fusion', 'satellite image'],
     classifiers = [],
 )
